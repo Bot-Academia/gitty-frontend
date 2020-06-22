fc794c294fb06d914751716b2819ecc44bc2d1cb
codetheorem <hrishikeshagarwalv@gmail.com>
2020-06-22 17:20:50 +0530

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+   <appheader></appheader>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import header from "./components/header.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    appheader: header,
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
