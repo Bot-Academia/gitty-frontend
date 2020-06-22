ef759b4e4c025dc943f1620706fb46d514593e96
codetheorem <hrishikeshagarwalv@gmail.com>
2020-06-22 18:50:20 +0530

    added intro

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
    <appheader></appheader>
+   <appintro></appintro>
+   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#edf057" fill-opacity="1" d="M0,256L1440,64L1440,0L0,0Z"></path></svg>
   </div>
 </template>
 
 <script>
 import header from "./components/header.vue";
+import intro from "./components/intro";
 
 export default {
   name: "App",
   components: {
     appheader: header,
+    appintro: intro,
   },
 };
 </script>
