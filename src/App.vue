29984732210ee124c43356ec8ad64485016186da
codetheorem <hrishikeshagarwalv@gmail.com>
2020-06-22 19:06:15 +0530

    ui issues

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
    <appheader></appheader>
    <appintro></appintro>
    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#edf057" fill-opacity="1" d="M0,256L1440,64L1440,0L0,0Z"></path></svg>
-   <features></features>
+   <features id="features"></features>
     <appfooter></appfooter>
   </div>
 </template>
