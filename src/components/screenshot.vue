2d622bac10f5c16edba0c79d554bff6d2310bf1f
Vinayak Sharma <54861487+vinayaksh42@users.noreply.github.com>
2020-06-23 20:11:13 +0530

    update

--- src/components/screenshot.vue
+++ src/components/screenshot.vue
@@ -4,29 +4,62 @@
       <div class="row">
         <div class="col-sm">
           <br /><br />
-          <div class="title">Easily Integrate your Github Org</div>
+          <div class="title">Easily Integrate your Github Account</div>
           <div class="desc">
-            Use our [git addorg] command to set the deafult org for your discord
-            server. In addition to this you can use the [git org] command for
-            displaying details of your org or any org in the world
+            Use our [git adduser (username)] to integrate your github account with
+            our bot gitty. It will display you several information related to your
+            github account on discord. You never have to login again and the account
+            will remain saved with us across all discord server which are running
+            Gitty! Run [git help-user] to know more.
           </div>
         </div>
         <div class="col-sm">
-          <img src="../assets/Discord_vbGm7r84po.png" class="img-thumbnail" />
+          <img src="../assets/Discord_ht7Rb5p7Wa.png" class="img-thumbnail" />
         </div>
       </div>
       <br /><br />
       <div class="row">
         <div class="col-sm">
-          <img src="../assets/Discord_ht7Rb5p7Wa.png" class="img-thumbnail" />
+         <img src="../assets/Discord_NqTRQoPvVa.png" class="img-thumbnail"/>
         </div>
         <div class="col-sm">
           <br /><br />
-          <div class="title">Know more about anyone on Github</div>
+          <div class="title">Know more about any Repo on Github</div>
           <div class="desc">
-            You can use the [git user] command to know more about anyone who
-            uses github. Gitty will display you a name, bio, location, public
-            repo and Url!
+            Check details of any repo on github using the repo-commands. You can check
+            the stars,forks,language,etc of any repo directly from discord. To know more
+            about this run [git help-repo] on your discord Server!
+          </div>
+        </div>
+      </div>
+      <br /><br />
+      <div class="row">
+        <div class="col-sm">
+          <br /><br />
+          <div class="title">See your organisation from discord now!</div>
+          <div class="desc">
+            We have specially made this discord bot keeping in mind the open
+            source organisations which are communicating over discord. Gitty
+            has a bunch of cool commands that will keep you updated with your
+            org. Run [git help-org] To know more about it!
+          </div>
+        </div>
+        <div class="col-sm">
+          <img src="../assets/Discord_EaDf2dXprq.png" class="img-thumbnail"/>
+        </div>
+      </div>
+      <br /><br />
+      <div class="row">
+        <div class="col-sm">
+          <img src="../assets/Discord_1AvbU6WR2w.png" class="img-thumbnail"/>
+        </div>
+        <div class="col-sm">
+          <br /><br />
+          <div class="title">Check in depth details of github repos!</div>
+          <div class="desc">
+            Use the [git repo-pull] or [git repo-issue] command for directly watching
+            your repo from a discord server. It provides you with all the
+            issues/prs of a repo. directly linking your workflow.
           </div>
         </div>
       </div>
@@ -43,21 +76,6 @@
         </div>
         <div class="col-sm">
           <img src="../assets/Discord_XgwsT9dxGu.png" class="img-thumbnail" />
-        </div>
-      </div>
-      <br /><br />
-      <div class="row">
-        <div class="col-sm">
-          <img src="../assets/Discord_Pemb2yYmpE.png" class="img-thumbnail" />
-        </div>
-        <div class="col-sm">
-          <br /><br />
-          <div class="title">See your repo from discord now!</div>
-          <div class="desc">
-            Use the [git pulls] or [git issues] command for directly watching
-            your repo from a discord server. It provides you with all the
-            issues/prs of a repo. directly linking your workflow.
-          </div>
         </div>
       </div>
     </div>
